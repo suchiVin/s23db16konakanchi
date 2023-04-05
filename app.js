741bc1fbf344db8cf8c90a02d66a5433c651071c
suchiVin <123125640+suchiVin@users.noreply.github.com>
2023-04-05 18:58:47 -0500

    pingpong

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var peacockRouter = require('./routes/peacock');
 var boardRouter = require('./routes/board')
+var selectorRouter = require('./routes/selector')
 
 
 
@@ -27,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/peacock', peacockRouter);
 app.use('/board', boardRouter);
+app.use('/selector',selectorRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
