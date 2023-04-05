75512a2d363f986a7840a5aafdaac8fbbdea1511
suchiVin <123125640+suchiVin@users.noreply.github.com>
2023-04-05 18:43:43 -0500

    added board and peacock

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var peacockRouter = require('./routes/peacock');
+var boardRouter = require('./routes/board')
+
+
 
 var app = express();
 
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/peacock', peacockRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
