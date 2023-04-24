ceb2e23208ac4c371aa2bc4531069930862c8785
suchiVin <123125640+suchiVin@users.noreply.github.com>
2023-04-23 22:12:11 -0500

    commit

--- models/peacock.js
+++ models/peacock.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const peacockSchema = mongoose.Schema({
-    peacock_species: String,
-    feather_color: String,
-    age: Number
+    peacock_species: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: [6, "peacock specie is not valid"],
+      },
+    feather_color: {
+        type: String,
+        required: true,
+        minLength: [9, "peacock color is not valid"],
+      },
+    age: {
+        type: Number,
+        required: true,
+        min: [9, "peacock age  in Number is not valid"],
+      },
 })
 module.exports = mongoose.model("peacock",
 peacockSchema)
--- routes/peacock.js
+++ routes/peacock.js
@@ -1,7 +1,18 @@
 var express = require('express');
-const peacock_controlers= require('../controllers/peacock');
+const peacock_controlers = require('../controllers/peacock');
 const peacock = require('../models/peacock');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET peacocks */
 router.get('/', peacock_controlers.peacock_view_all_Page);
@@ -10,7 +21,7 @@ router.get('/detail', peacock_controlers.peacock_view_one_Page);
 /* GET create peacock page */
 router.get('/create', peacock_controlers.peacock_create_Page);
 /* GET create update page */
-router.get('/update', peacock_controlers.peacock_update_Page);
+router.get('/update', secured, peacock_controlers.peacock_update_Page);
 /* GET delete peacock page */
 router.get('/delete', peacock_controlers.peacock_delete_Page);
 module.exports = router;
